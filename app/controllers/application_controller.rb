727d0ee6786d9d1f150c98d543ae6f6a1bc91ef4
julia <julia.tena.domingo@estudiantat.upc.edu>
2024-04-16 22:25:47 +0200

    arreglo barra cercador

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "<h1>It works WASLAB04!</h1>".html_safe
   end
+
+  before_action :initialize_search
+
+  def initialize_search
+    session[:last_search] ||= ''
+  end
 end
