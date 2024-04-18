9eafc8f568e996ac2382d693626c38b832260004
albalr <albalopez656@gmail.com>
2024-04-18 09:06:32 +0200

    restriccions edit delete create posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   # POST /comments or /comments.json
   def create
     @comment = Comment.new(comment_params)
-    @comment.user_id = 1
+    @comment.user_id = current_user.id
 
     respond_to do |format|
       if @comment.save
