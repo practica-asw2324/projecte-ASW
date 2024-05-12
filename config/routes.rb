43987d62568ab311b9aedb5b9cf26b5cd7272ed2
albalr <albalopez656@gmail.com>
2024-05-12 21:47:27 +0200

    especificacio quasi feta, menys replies

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,7 @@ Rails.application.routes.draw do
     get 'login', to: 'users#new', as: 'new_user'
   end
 
-  resources :comments do
-    put 'like', on: :member
-    put 'dislike', on: :member
-  end
-
-  resources :magazines
   resources :users
-
 
   resources :magazines do
     member do
@@ -30,11 +23,14 @@ Rails.application.routes.draw do
     put 'like', on: :member
     put 'dislike', on: :member
     put 'boost', on: :member
-
+    resources :comments, only: [:create, :index, :destroy, :update] do
+      put 'like', on: :member
+      put 'dislike', on: :member
+    end
   end
 
-    get 'new_link', to: 'posts#new', type: 'link', as: :new_link
-    get 'new_thread', to: 'posts#new', type: 'thread', as: :new_thread
+  get 'new_link', to: 'posts#new', type: 'link', as: :new_link
+  get 'new_thread', to: 'posts#new', type: 'thread', as: :new_thread
 
   root 'posts#index'
 end
